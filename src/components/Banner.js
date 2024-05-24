ecc2408fda3303cfcdb4b11f6fa6192b597850cb
yjHwang00 <yjhwang@zenithn.com>
2024-05-24 14:25:10 +0900

    local push
    local push

---
+++ src/components/Banner.js
@@ -0,0 +1,42 @@
+// imp
+import { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import headerImg from "../assets/img/header-img.svg";
+
+// rafc, rafce(분리)
+export const Banner = () => {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const toRotate = ["Web Developer", "Web Designer", "UI/UX Designer"];
+  const [text, setText] = useState("");
+  const period = 2000;
+
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="align-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <span className="tagline">Welcome to my Portfolio</span>
+            <h1>
+              {`Hi I'm webdecoded`}
+              <span className="wrap">web developer</span>
+            </h1>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </p>
+            <button onClick={() => console.log("connect")}>
+              Let's connect
+            </button>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <img src={""} alt="Header Img" />
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
